881ae56e602343a7180b94c99a97eddb229b9a1b
Carlos O'Ryan <coryan@google.com>
2026-08-27 17:34:23 +0000

    cleanup: rename `packages/*` directories  (#548)
    Make the directories match their future split repositories.

--- Package.swift
+++ Package.swift
@@ -24,13 +24,13 @@ let package = Package(
     .library(name: "GoogleType", targets: ["GoogleType"])
   ],
   dependencies: [
-    .package(path: "../../packages/wkt")
+    .package(path: "../../packages/swift-google-wkt")
   ],
   targets: [
     .target(
       name: "GoogleType",
       dependencies: [
-        .product(name: "GoogleCloudWKT", package: "wkt")
+        .product(name: "GoogleCloudWKT", package: "swift-google-wkt")
       ],
     )
   ]
