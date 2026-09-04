b7f9b4b5c86d588e6d23cc1de1247e44daaeda5a
Jeff Ching <chingor@google.com>
2026-09-04 11:18:05 -0700

    deps: update wkt, auth dependencies to remote url (#545)
    Towards #450
    
    Also, updates the CI script to be able to use local code for testing.
    Example found failure: in #618

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     .library(name: "GoogleType", targets: ["GoogleType"])
   ],
   dependencies: [
-    .package(path: "../../packages/swift-google-wkt")
+    .package(url: "https://github.com/googleapis/swift-google-wkt", from: "0.1.0-preview")
   ],
   targets: [
     .target(
